787d858682014a5b6170517388875d2203a156b4
a1a2a3a4a5 <linub7@gmail.com>
2022-10-30 17:38:38 +0330

    initial settings & add tailwind and custom colors

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
+const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
